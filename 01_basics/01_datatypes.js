af2da452f2faac9937738f13e44c1f1d54c8779d
Abhishek Hadimani <108976136+4bhish@users.noreply.github.com>
2023-10-31 05:26:35 +0000

    testing datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,17 @@
+/* 
+number 
+string
+boolean
+undefined
+null
+object
+*/
+
+const myAge = 23;
+const myName = 'abhishek';
+const booleanExample = myAge === myName
+
+
+console.log(typeof booleanExample);
+
+
