524a534d4b33ec68d1535fbc8110bda2e22684d8
Abhishek Hadimani <108976136+4bhish@users.noreply.github.com>
2023-10-31 11:44:43 +0000

    Understanding Stack and Heap

--- 01_basics/05_dataTypesSummery.js
+++ 01_basics/05_dataTypesSummery.js
@@ -5,11 +5,11 @@
 const uniqueId = Symbol(124)
 const anotherUniqueId = Symbol(124)
 
-console.log(uniqueId === anotherUniqueId);
+// console.log(uniqueId === anotherUniqueId);
 
 const bigIntExample = 23532222222222222222222222222222222n;
 
-console.log(typeof bigIntExample);
+// console.log(typeof bigIntExample);
 
 const myFunc = function(){
     console.log('Abhishek');
@@ -19,4 +19,25 @@ console.log(typeof myFunc)
 
 let arr = ['helo','bitch','world']
 
-console.log(typeof arr);
+// console.log(typeof arr);
+
+// Stack and Heap Memory
+
+// Stack (primitive)
+let userId = 1245;
+let anotherId = userId;
+
+anotherId = 223
+// console.log(userId);
+
+// Heap (Non-Primitive)
+
+let userOne = {
+    name:'satyapal',
+    age:23
+}
+
+let userTwo = userOne
+userTwo.name = "abhishek";
+
+console.log(userOne);
