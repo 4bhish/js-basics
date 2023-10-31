1daf3b4e2b043dad83dae6bc3ba1539759946ccd
Abhishek Hadimani <108976136+4bhish@users.noreply.github.com>
2023-10-31 05:19:24 +0000

    Update 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1234;
+let accountEmai = 'abhishek@facebook.com';
+var accountPassword = 123432;
+accountcity = 'bhiwandi';
+
+let accountState;
+
+
+accountEmai = 'abhish@facebook.com';
+accountPassword = 23222;
+accountcity = 'solapur';
+
+console.table([accountEmai,accountPassword,accountcity,accountState])
+
