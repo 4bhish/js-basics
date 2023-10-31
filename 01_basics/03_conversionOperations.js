80ad0ab5ab21ea429dddd055a78f74f78307bd99
Abhishek Hadimani <108976136+4bhish@users.noreply.github.com>
2023-10-31 10:49:17 +0000

    Update 03_conversionOperations.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -6,4 +6,15 @@ let isLoggedIn = 0;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log( booleanIsLoggedIn)
+// console.log( booleanIsLoggedIn)
+
+/* Operations */
+
+
+let value = 3;
+let negValue =  -value;
+
+console.log(2+2)
+
+let op1 = "2" + 2+2;
+console.log( op1);
