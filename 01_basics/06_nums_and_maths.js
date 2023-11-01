33b09d01cf6127275eddbc34017b21f15c040730
Abhishek Hadimani <108976136+4bhish@users.noreply.github.com>
2023-11-01 10:35:22 +0000

    practise Nums and Math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,18 @@
+const score = 100;
+
+// console.log(score)
+
+const balence = new Number(100000)
+
+// console.log(balence.toLocaleString('en-IN'))
+
+// console.log(Math.round(2.9))
+
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.1))
+
+const min =20
+const max = 40
+// console.log(Math.round(Math.random()*100))
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
