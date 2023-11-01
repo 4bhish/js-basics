60f1bf210e17d73ab4abb835ecae93fbc32668e8
Abhishek Hadimani <108976136+4bhish@users.noreply.github.com>
2023-11-01 15:39:36 +0000

    Update 02_basics

---
+++ 02_basics/02_array.js
@@ -0,0 +1,10 @@
+const myArr = ["abhishek","alkesh","santosh"]
+
+// console.log("print array", myArr)
+
+// console.log( myArr.slice(0,2))
+// console.log(myArr);
+
+console.log('print arr', myArr.splice(1,2))
+
+console.log(myArr)
