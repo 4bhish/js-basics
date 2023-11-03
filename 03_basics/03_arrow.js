c18b9c7d58f240ef0c306c9b10b0839c108b0101
Abhishek Hadimani <108976136+4bhish@users.noreply.github.com>
2023-11-03 02:47:12 +0000

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -2,8 +2,16 @@ const user ={
     userName : 'Abhishek',
     age:22,
     welcomeMsg : function (){
-        console.log(`Welcome Mr. ${this.userName}`)
+        console.log(`Welcome Mr. ${userName}`)
     }
 }
 
-user.welcomeMsg()
+// user.welcomeMsg()
+
+
+
+const printMyName = () => {
+    console.log(this)
+}
+
+printMyName()
