e1766aab4f596cc0c61366d2b35ce6c0f0557055
Abhishek Hadimani <108976136+4bhish@users.noreply.github.com>
2023-11-03 02:27:04 +0000

    Practise Scopes

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,38 @@
+
+
+if(true){
+    let a = 10;
+    const b = 100;
+    var c = 1000;
+}
+
+// console.log(c);
+
+function parentFunc(){
+    const username ='abhishek';
+
+    function childFunc(){
+        const name = 'yoabhish';
+        console.log(username)
+    }
+
+    console.log(name)
+
+    childFunc()
+}
+
+
+// Mini Hoisting
+
+
+function addtwonums(a,b){
+    console.log(a + b)
+}
+
+addTwoNums(3,7)
+
+const addTwoNums = function addtwoNums(a,b){
+    console.log(a+b)
+}
+
+
